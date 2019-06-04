c48ac579d029e7abd22cc7d7fbbf8f305473ec22
stravze <stravze@gmail.com>
2019-06-04 13:50:09 +0100

    adding subscription var to azurerm provider

--- 00-init.tf
+++ 00-init.tf
@@ -1,15 +1,16 @@
 provider "azurerm" {
-  version         = "=1.24.0"
+  version                           = "=1.24.0"
+  subscription_id                   = "${var.subscription_id}"
 }
 
 provider "http" {
-  version         = ">=1.0"
+  version                           = ">=1.0"
 }
 
 provider "null" {
-  version         = ">=2.1"
+  version                           = ">=2.1"
 }
 
 provider "template" {
-  version         = ">=2.1"
+  version                           = ">=2.1"
 }
