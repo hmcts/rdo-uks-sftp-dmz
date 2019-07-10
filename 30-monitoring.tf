77cd249ceeb214c56c533e8245d9e1ccd775a050
stravze <stravze@gmail.com>
2019-07-10 10:33:23 +0100

    updating resource id

--- 30-monitoring.tf
+++ 30-monitoring.tf
@@ -9,7 +9,7 @@ resource "azurerm_monitor_metric_alertrule" "cpu" {
 
   enabled                                   = true
 
-  resource_id                               = "${element(azurerm_virtual_machine.dmz.*.id, count.index)}" 
+  resource_id                               = "${azurerm_virtual_machine.dmz.*.id}" 
   metric_name                               = "Percentage CPU"
   operator                                  = "GreaterThan"
   threshold                                 = 75
