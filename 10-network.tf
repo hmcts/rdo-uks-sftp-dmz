f8433e7bb097eed60892cf1f7803b77c65f47560
gunnertwin <daniel@alvestechnology.io>
2019-08-14 09:41:12 +0100

    Change SFTP cidr

--- 10-network.tf
+++ 10-network.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "vnet" {
     name                              = "${var.name}-vnet"
-    address_space                     = ["${var.vnet}60.0/22"]
+    address_space                     = ["${var.vnet}192.0/22"]
     location                          = "${var.location}"
     resource_group_name               = "${data.azurerm_resource_group.rg.name}"
     tags                              = "${var.tags}"
@@ -11,7 +11,7 @@ resource "azurerm_subnet" "subnet_public" {
     name                              = "${var.name}-public-subnet"
     resource_group_name               = "${data.azurerm_resource_group.rg.name}"
     virtual_network_name              = "${azurerm_virtual_network.vnet.name}"
-    address_prefix                    = "${var.vnet}60.0/24"
+    address_prefix                    = "${var.vnet}192.0/24"
     service_endpoints                 = ["Microsoft.Storage"]
     #network_security_group_id         = "${azurerm_network_security_group.public_nsg.id}"
     lifecycle { 
@@ -23,7 +23,7 @@ resource "azurerm_subnet" "subnet_mgmt" {
     name                              = "${var.name}-mgmt-subnet"
     resource_group_name               = "${data.azurerm_resource_group.rg.name}"
     virtual_network_name              = "${azurerm_virtual_network.vnet.name}"
-    address_prefix                    = "${var.vnet}61.0/24"
+    address_prefix                    = "${var.vnet}193.0/24"
    # network_security_group_id         = "${azurerm_network_security_group.public_nsg.id}"
     lifecycle { 
          ignore_changes               = ["route_table_id"]
