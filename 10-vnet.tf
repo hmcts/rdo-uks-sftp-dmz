c1253f96de8f89aba46c5e74afcf92490947841f
stravze <stravze@gmail.com>
2019-08-27 11:13:57 +0100

    fixing vnet lookup

--- 10-vnet.tf
+++ 10-vnet.tf
@@ -1,5 +1,5 @@
 resource "azurerm_virtual_network" "vnet" {
-    name                              = "${data.azurerm_resource_group.dmz.name}"
+    name                              = "${data.azurerm_virtual_network.name}"
     address_space                     = ["${data.azurerm_virtual_network.vnet-dmz.address_space}"]
     location                          = "${var.location}"
     resource_group_name               = "${data.azurerm_resource_group.rg.name}"
