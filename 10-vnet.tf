121aba2e99d7392196e10054d586fa96658f6555
stravze <stravze@gmail.com>
2019-08-27 10:57:32 +0100

    reconfig

--- 10-vnet.tf
+++ 10-vnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "vnet" {
     name                              = "${data.azurerm_resource_group.dmz.name}"
-    address_space                     = ["${data.azurerm_resource_group.dmz.address_space}"]
+    address_space                     = ["${data.azurerm_virtual_network.vnet-dmz.address_space}"]
     location                          = "${var.location}"
     resource_group_name               = "${data.azurerm_resource_group.rg.name}"
     tags                              = "${var.tags}"
