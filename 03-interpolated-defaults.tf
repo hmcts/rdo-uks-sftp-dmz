acd84bc9e3ffba8d9db13866ff0bc99112103b35
stravze <stravze@gmail.com>
2019-09-10 14:44:06 +0100

    route change added

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -43,6 +43,12 @@ data "azurerm_subnet" "subnet-dmz-mgmt" {
   resource_group_name                       = "${data.azurerm_resource_group.dmz.name}"
 }
 
+data "azurerm_subnet" "subnet-palo-public" {
+  name                                      = "dmz-palo-public"
+  virtual_network_name                      = "${data.azurerm_resource_group.dmz.name}"
+  resource_group_name                       = "${data.azurerm_resource_group.dmz.name}"
+}
+
 data "azurerm_network_security_group" "sg-nsg-sftp" {
   name                                      = "nsg_sftp"
   resource_group_name                       = "${data.azurerm_resource_group.dmz.name}"
