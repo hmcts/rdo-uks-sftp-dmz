0932aa2352d59b5f01159b911f7c13dc031d9f52
gunnertwin <daniel@alvestechnology.io>
2019-09-11 13:11:53 +0100

    change static route to point to palo public address prefix

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -49,6 +49,12 @@ data "azurerm_subnet" "subnet-palo-public" {
   resource_group_name                       = "${data.azurerm_resource_group.dmz.name}"
 }
 
+data "azurerm_subnet" "subnet-palo-private" {
+  name                                      = "dmz-palo-private"
+  virtual_network_name                      = "${data.azurerm_resource_group.dmz.name}"
+  resource_group_name                       = "${data.azurerm_resource_group.dmz.name}"
+}
+
 data "azurerm_network_security_group" "sg-nsg-sftp" {
   name                                      = "nsg_sftp"
   resource_group_name                       = "${data.azurerm_resource_group.dmz.name}"
