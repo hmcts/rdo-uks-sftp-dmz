556ca30480b8e1a57cb152a5b73490a2befe0d94
stravze <stravze@gmail.com>
2019-11-29 14:25:43 +0000

    missing quote

--- 11-vnet.tf
+++ 11-vnet.tf
@@ -2,7 +2,7 @@ resource "azurerm_virtual_network" "vnet_sftp" {
   name                            = "${var.rg_name}-vn"
   location                        = "${azurerm_resource_group.rg_sftp.location}"
   resource_group_name             = "${azurerm_resource_group.rg_sftp.name}"
-  address_space                   = ["10.97.0.0/24]
+  address_space                   = ["10.97.0.0/24"]
 
   tags                            = var.common_tags
 }
