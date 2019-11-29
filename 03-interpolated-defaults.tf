007ffa312d093bcc37c1b831b26b51c7dce8888b
stravze <stravze@gmail.com>
2019-11-29 13:12:58 +0000

    untangling

--- 03-interpolated-defaults.tf
+++ 03-interpolated-defaults.tf
@@ -1,32 +1,3 @@
 
-data "azurerm_key_vault_secret" "admin-password" {
-name                                        = "admin-password"
-vault_uri                                   = "${var.key_vault_uri}"
-}
 
-data "azurerm_key_vault_secret" "admin-username" {
-name                                        = "admin-username"
-vault_uri                                   = "${var.key_vault_uri}"
-}
 
-data "azurerm_key_vault_secret" "smtp_email_address" {
-name                                        = "smtp-email-address"
-vault_uri                                   = "${var.key_vault_uri}"
-}
-
-data "azurerm_key_vault_secret" "smtp_password" {
-name                                        = "smtp-password"
-vault_uri                                   = "${var.key_vault_uri}"
-}
-
-/*
-
-data "azurerm_network_interface" "proxy_private_ip" {
-  name                                      = "proxy-sbox-nic"
-  resource_group_name                       = "${data.azurerm_resource_group.dmz.name}"
-}
-
-locals {
-  default_gateway                           = "${cidrhost(data.azurerm_subnet.subnet-dmz-sftp.address_prefix,1)}"
-}
-*/
--- 11-vnet.tf
+++ 11-vnet.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network" "vnet_sftp" {
-  name                            = "${var.vnet_name}"
+  name                            = "${var.rg_name}-vn"
   location                        = "${azurerm_resource_group.rg_sftp.location}"
   resource_group_name             = "${azurerm_resource_group.rg_sftp.name}"
-  address_space                   = ["${var.vnet_cidr}"]
+  address_space                   = ["10.97.0.0/24]
 
   tags                            = var.common_tags
 }
